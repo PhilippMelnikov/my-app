a6fbb300a991309941660950661ae5a23f04f38c
Philipp Melnikov <fmelnikov123@gmail.com>
2017-07-14 10:27:43 +0300

    edit-item-modal change the way data is handled

--- src/app/edit-item-modal/edit-item-modal.component.ts
+++ src/app/edit-item-modal/edit-item-modal.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
-import { FormControl, FormGroup } from '@angular/forms';
 import { Item } from '../models/item';
 
 @Component({
@@ -15,24 +14,11 @@ export class EditItemModalComponent implements OnInit {
   @Input() categories: any[];
   @Input() itemToEdit: any;
 
-  newItemForm: FormGroup = new FormGroup({
-    category: new FormControl(),
-    title: new FormControl(),
-    selling_price: new FormControl(),
-    purchase_price: new FormControl(),
-  });
 
   constructor() { }
 
   ngOnInit() {
-    this.myModal.onOpen.subscribe(() => {
-      this.newItemForm.setValue({
-        category: this.itemToEdit ? this.itemToEdit.category : null,
-        title: this.itemToEdit ? this.itemToEdit.title : null,
-        selling_price: this.itemToEdit ? this.itemToEdit.selling_price : null,
-        purchase_price: this.itemToEdit ? this.itemToEdit.purchase_price : null,
-       })
-    })
+
   }
 
 }
